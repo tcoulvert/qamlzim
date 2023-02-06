a5b887546258fb059d183728f2c281dc3bb9aa7c
Thomas Sievert <tcsievert@gmail.com>
2023-02-05 20:39:24 -0800

    updated setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="qamlzim",
-    version="1.3.0",
+    version="1.0.0",
     description="Binary Classifier trained with D-Wave's Quantum Annealers.",
     packages=find_packages(include=["qamlzim"]),
     classifiers=[
